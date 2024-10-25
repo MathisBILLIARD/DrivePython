9a6e7187287af50e4f1be055553da81da1408043
PaulD <paul.dailly@student.junia.com>
2024-10-25 14:42:08 +0200

    code poal

--- CloudDrive/settings.py
+++ CloudDrive/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'ENGINE': 'django.db.backends.mysql',
     'NAME': 'django',
     'USER': 'root', 
-    'PASSWORD':'Hadrigaming62',
+    'PASSWORD':'Polda510Polda510',
     'HOST': '127.0.0.1', 
     'PORT': '3306', 
     }
