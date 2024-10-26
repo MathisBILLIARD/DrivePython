15f3fb97f64b8aca3cddaedf37cec8e637a48e50
Hadrien62 <hadrien.delobel@student.junia.com>
2024-10-26 15:41:35 +0200

    taille

--- auth_app/templatetags/custom_filter.py
+++ auth_app/templatetags/custom_filter.py
@@ -11,9 +11,9 @@ def filesize_display(file_size, unit=''):
     if file_size < 1024:
         return f"{file_size} octets"
     elif file_size < 1048576:
-        return f"{round(file_size / 1000)} ko"  # Divise par 1024 et garde 2 chiffres significatifs
+        return f"{file_size / 1000:.2f} ko"  # Divise par 1024 et garde 2 chiffres significatifs
     else:
-        return f"{round(file_size / 1000000)} Mo"  # Divise par 1048576 et garde 2 chiffres significatifs
+        return f"{file_size / 1000000:.2f} Mo"  # Divise par 1048576 et garde 2 chiffres significatifs
 
 @register.filter
 def foldername_display(folder_path):
@@ -21,3 +21,18 @@ def foldername_display(folder_path):
     Extrait le nom du dossier à partir du chemin complet.
     """
     return folder_path.split('/')[-2]
+
+@register.filter
+def sum_files_size(files):
+    """
+    Calcule la taille totale des fichiers.
+    """
+    return sum([file.file_size for file in files])
+
+@register.filter
+def usage_percentage(file_size, max_size=100 * 1024 * 1024):  # Par défaut, 100 Mo en octets
+    """
+    Calcule le pourcentage d'utilisation par rapport à la taille max.
+    """
+    percentage = (file_size / max_size) * 100
+    return round(min(percentage, 100), 2)  # Limite à 100 % max et arrondi à 2 décimales
